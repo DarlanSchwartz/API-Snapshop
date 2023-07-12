9d07ec86527401d39fba4f09e4226eec9ab0b5b9
RayAra2004 <raynan.araujo@gmail.com>
2023-07-12 17:24:55 -0300

    fix: withdrawing authorization to login

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -7,7 +7,7 @@ import { SignInSchema, SignUpSchema } from "../schemas/account.schemas.js";
 const accountRouter = Router();
 
 accountRouter.post('/cadastro', validateAuth, validateSchema(SignUpSchema), signUp);
-accountRouter.post('/login', validateAuth, validateSchema(SignInSchema), signIn);
+accountRouter.post('/login', validateSchema(SignInSchema), signIn);
 accountRouter.post('/logout', validateAuth, logout);
 
 export default accountRouter;
