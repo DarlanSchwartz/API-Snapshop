e0adc1c3ac8d733942b84269574b43ed812184fa
TamaraFerr <tamarablacksmith@hotmail.com>
2023-07-12 16:50:38 -0300

    feat: signIn adn signUp working successfully

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,1 +1,13 @@
 import { Router } from "express";
+import validateAuth from "../middlewares/validateAuth.js";
+import { signUp, signIn, logout } from "../controllers/account.controller.js";
+import validateSchema from "../middlewares/validadeSchema.js";
+import { SignInSchema, SignUpSchema } from "../schemas/account.schemas.js";
+
+const accountRouter = Router();
+
+accountRouter.post('/cadastro', validateAuth, validateSchema(SignUpSchema), signUp);
+accountRouter.post('/login', validateAuth, validateSchema(SignInSchema), signIn);
+accountRouter.post('/logout', validateAuth, logout);
+
+export default accountRouter;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import productRouter from "./product.routes.js";
+import accountRouter from "./account.routes.js"
 
 const router = Router();
 
+router.use(accountRouter);
 router.use(productRouter);
+
 
 export default router;
